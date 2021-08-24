426a5f31bbac801a8c7cdc61b5a9f76f292d6522
name <arielkim429@gmail.com>
2021-08-25 03:22:33 +0900

    feat: Queen 이동 구현

--- src/main/java/domain/piece/Queen.java
+++ src/main/java/domain/piece/Queen.java
@@ -1,14 +1,31 @@
 package domain.piece;
 
+import domain.Direction;
 import domain.player.Player;
 import domain.position.Position;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Queen extends Piece {
 
     private static final String BLACK_QUEEN_UNICODE = "\u265B";
     private static final String WHITE_QUEEN_UNICODE = "\u2655";
+    private static final List<Direction> movingDirections;
+
+    static {
+        movingDirections = Arrays.asList(
+                Direction.NORTH,
+                Direction.EAST,
+                Direction.SOUTH,
+                Direction.WEST,
+                Direction.NORTH_EAST,
+                Direction.NORTH_WEST,
+                Direction.SOUTH_EAST,
+                Direction.SOUTH_WEST);
+    }
 
     private Queen(Position position, Player player) {
         super(position, player);
@@ -20,12 +37,28 @@ public class Queen extends Piece {
 
     @Override
     protected Boolean checkMovingPolicy(Position target, Map<Position, PieceDto> boardDto) {
-        return null;
+        Direction direction = Direction.getDirection(position, target);
+
+        if (!movingDirections.contains(direction)) {
+            return false;
+        }
+
+        int fileDirection = direction.getFileDirection();
+        int rankDirection = direction.getRankDirection();
+
+        Position startPathPosition = position.getMovedPosition(fileDirection, rankDirection);
+        for (; !startPathPosition.equals(target); startPathPosition = startPathPosition.getMovedPosition(fileDirection, rankDirection)) {
+            if (!Objects.isNull(boardDto.get(startPathPosition))) {
+                return false;
+            }
+//                paths.add(startPathPosition);
+        }
+        return true;
     }
 
     @Override
     protected PieceState makePieceState() {
-        return null;
+        return this;
     }
 
     @Override
